63b53270b18f478ec37137748ead24dc5012b08f
Ubuntu <ubuntu@ip-172-31-42-92.ec2.internal>
2026-03-08 01:02:01 +0000

    commit teste debug

--- src/config/app_config.py
+++ src/config/app_config.py
@@ -1,11 +1,16 @@
 import os
 
 class AppConfig:
-
     def __init__(self):
+        # Usamos o caminho absoluto direto do seu ambiente Cloud9 para evitar erros de lógica
+        base_dir = "/home/ubuntu/environment/projeto_pyspark"
+        
+        print(f"DEBUG - Caminho Base: {base_dir}")
 
-        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-        self.pedidos_path = os.path.join(base_dir, "data/input/pedidos/*.csv.gz")
-        self.pagamentos_path = os.path.join(base_dir, "data/input/pagamentos/*.json.gz")
-        self.output_path = os.path.join(base_dir, "data/output/relatorio_recusados_2025")
+        # Agora montamos os caminhos a partir da raiz correta
+        self.pedidos_path = os.path.join(base_dir, "data", "input", "pedidos", "*.csv.gz")
+        self.pagamentos_path = os.path.join(base_dir, "data", "input", "pagamentos", "*.json.gz")
+        self.output_path = os.path.join(base_dir, "data", "output", "relatorio_recusados_2025")
+        
+        print(f"DEBUG - Procurando Pedidos em: {self.pedidos_path}")
+        print(f"DEBUG - Procurando Pagamentos em: {self.pagamentos_path}")
